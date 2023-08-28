00d28ecf8fd8753b0974af35c3335519fc110fcf
shahadat10033 <shahadat10033@gmail.com>
2023-08-28 19:03:24 +0600

    news.json file added in the server

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const cors = require("cors");
+const newsArray =require( "./news.json")
 const app = express()
-const port = 3000
+const port = 6001
+console.log(newsArray)
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+app.get('/news', (req, res) => {
+  res.send(newsArray)
 })
 
 app.listen(port, () => {
